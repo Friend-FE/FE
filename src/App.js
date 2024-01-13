165f1622bc636e4063770a80bb086e67cf6a2100
dani001024 <aj9473@naver.com>
2024-01-13 14:37:37 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-// const App = ()=>{
-//   return (
-//     <div>
-//       <h1>hello</h1>
-//     </div>
-//   );
-// }
-
-import Footer from "./components/footer";
-
-// export default App;
 const App = ()=>{
   return (
-    <Footer></Footer>
+    <div>
+      <h1>hello</h1>
+    </div>
   );
 }
 
