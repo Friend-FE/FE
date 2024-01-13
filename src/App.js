2cb0b4688a99e259f23f158eca60428c0c464a8d
dani001024 <aj9473@naver.com>
2024-01-13 14:34:30 +0900

    header 컴포넌트 UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+// const App = ()=>{
+//   return (
+//     <div>
+//       <h1>hello</h1>
+//     </div>
+//   );
+// }
+
+import Footer from "./components/footer";
+
+// export default App;
 const App = ()=>{
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Footer></Footer>
   );
 }
 
 export default App;
+
