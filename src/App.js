c83bc68f3c661e0dff4d63b31aa042efdb4c4c81
linjuuu <jun1_id@naver.com>
2024-01-19 19:05:08 +0900

    원형 체크박스 컴포넌트 생성과 회원가입 3 페이지, 이메일 인증 페이지 작성을 했습니다.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import MatchingHistory from './components/MyPage_Component/MatchingHistory';
 import MembershipWithdrawal from './components/MyPage_Component/MembershipWithdrawal';
 import ModifyingInfo from './components/MyPage_Component/ModifyingInfo';
 import ProfileCard from './components/MyPage_Component/ProfileCard';
-
+import CertifyEmail from './pages/Certify/CertifyEmail';
+import SignupInfo from './pages/Signup/SignupInfo';
 
 const App = ()=>{
 	return (
 	  <div>
 			<Routes>
 				<Route path="/MyPage" element={<MyPage/>}/>
+
 				<Route path="/MyPage/DeactivateAccount" element={<DeactivateAccount/>}/>
 				<Route path="/MyPage/MatchingHistory" element={<MatchingHistory/>}/>
 				<Route path="/MyPage/MembershipWithdrawal" element={<MembershipWithdrawal/>}/>
 				<Route path="/MyPage/ModifyingInfo" element={<ModifyingInfo/>}/>
 				<Route path="/MyPage/ProfileCard" element={<ProfileCard/>}/>
+				<Route path="/CertifyEmail" element={<CertifyEmail/>}/> 
+				<Route path="/SignupInfo" element={<SignupInfo/>}/> 
 			</Routes>
 	  </div>
 	);
---
+++ src/pages/Certify/CertifyEmail.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import Header from '../../components/header/index'
+import Footer from '../../components/footer/index'
+import styled from 'styled-components';
+import Title from '../../components/title';
+
+const CertifyEmail = () => {
+
+    const [email, setEmail] = useState('');
+    const [verificationCode, setVerificationCode] = useState('');
+
+    const isEmailFormValid = email !== '';
+    const isVerificationCodeFormValid = verificationCode !== '';
+
+    const handleEmailSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submitted:', email);
+    }; 
+
+    const navigate = useNavigate();
+    const handleVerificationCodeSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submitted:', verificationCode);
+
+        //코드가 일치하는 경우 다음페이지로 넘어가는 navigate 코드 필요 (api 호출로 조건문 필요)
+        //navigate('/CertifyEnd');
+    }; 
+
+    return (
+        <>
+            <Header/>
+            <AppContainer>
+                    <Title title = "회원가입"/>
+                    <RoundedBox>
+                        <DiscriptionText>부경대학교 학교 이메일을 입력해주세요.</DiscriptionText>
+                        <InputField type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <SubmitButton disabled={!isEmailFormValid} onClick={handleEmailSubmit}>인증번호 전송</SubmitButton>
+                        
+                        <DiscriptionText>이메일로 온 인증번호를 입력해주세요.</DiscriptionText>
+                        <InputField type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
+                        <SubmitButton disabled={!isVerificationCodeFormValid} onClick={handleVerificationCodeSubmit}>확인</SubmitButton>
+                    </RoundedBox>
+            </AppContainer>
+            <Footer/>
+        </>
+    )
+} 
+export default CertifyEmail;
+
+// 전체를 담고 있는 컨테이너
+const AppContainer = styled.div`
+  height: 80vh;
+  width: 40%; /* 원하는 크기로 조정 가능 */
+  margin: 0 auto; /* 수평 가운데 정렬을 */
+`;
+
+// 네모 박스 그리기 
+const RoundedBox = styled.div`
+  border: 2px solid #23CAFF; 
+  border-radius: 10px; /* 모서리를 둥글게 */
+  padding: 40px; /* 네모박스 내부 여백 */
+  margin-top : 100px;
+`;
+
+//무엇을 입력해야하는지 설명하는 텍스트
+const DiscriptionText = styled.div`
+  font-size: 25px;
+  margin : 20px;
+`;
+
+
+const InputField = styled.input`
+  width: 70%;
+  height : 30px;
+  padding: 8px;
+  margin-right: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #23CAFF;
+  width : 20%;
+  height : 50px;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+
+  /* 입력 안했을 때 */
+  &:disabled {
+    background-color: #B9EEFF;
+    cursor: not-allowed;
+  }
+`;
