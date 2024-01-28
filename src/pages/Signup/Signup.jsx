381889e0c861b431e713ac4c1551d64d485fe439
dani001024 <aj9473@naver.com>
2024-01-28 23:20:21 +0900

    회원가입 사진, 텍스트 api 요청하기 위해 수정

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,157 +1,332 @@
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import Title from '../../components/title';
-import CircleCheckbox from '../../components/CircleCheckbox/CircleCheckbox';
-import styled from 'styled-components';
-import ProfileImage from './ProfileImage';
-import Footer from '../../components/footer';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Title from "../../components/title";
+import CircleCheckbox from "../../components/CircleCheckbox/CircleCheckbox";
+import styled from "styled-components";
+import ProfileImage from "./ProfileImage";
+import Footer from "../../components/footer";
+import axios from "axios";
+import LimitInputForm from "../../components/LimitInputForm/LimitInputForm";
 
 const Signup = () => {
-    //로그인 정보 관리
-    const navigate = useNavigate();
-    const [passwordCheck, setPasswordCheck] = useState('');
-    const [wrongPW, setWrongPW] = useState(false);
+  //로그인 정보 관리
+  const navigate = useNavigate();
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [wrongPW, setWrongPW] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    role: 0,
+    nickname: "",
+    phone: "",
+    birthday: "",
+    gender: 0,
+    height: 0,
+    region: "",
+    department: "",
+    distance: 0,
+    smoking: 0,
+    drinking: 0,
+    introduction: "",
+    preference: "",
+    nonRegion: "",
+    nondepartment: "",
+    nonstudentid: "",
+    nonage: "",
 
-    const [values, setValues] = useState({
-        email: "",
-        password: "",
-        nickname: "",
-        role:0,
-        phone :"",
-        birthday:"",
-        gender:0,
-        height: 0,
-        region: "",
-        department:"",
-        distance:0,
-        smoking:0,
-        drinking:0,
-        introduction:"",
-        preference:"",
-        nondepartment:"",
-        nonstudentid:"",
-        nonage:"",
-        nonregion:""
-      })
+  });
+
+
+  const handleFileChange = (file) => {
+    setSelectedFile(file);
+  };
+
+  const checkWrongPW = (e) => {
+    setPasswordCheck(e.target.value);
+    if (values.password !== passwordCheck) {
+      setWrongPW(false);
+    } else setWrongPW(true);
+  };
+
+  const handleChange = (e) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("실행");
+    alert(JSON.stringify(values, null, 2));
+  };
+
+
+  const handleCancleButton = (event) => {
+    //취소 시 이전 페이지로
+    navigate(-1);
+  };
+
+  const handleSubmitButton = (event) => {
+    //console.log(values);
+    //가입완료, 정보들 서버로 전송하는 api 필요
+
+
+    event.preventDefault();
+    if (true) { //selectedFile
+      const formData = new FormData();
+      formData.append("image", selectedFile);
+
+
+      const request = {
+
+          email: "123456@gmail.com",
+          password: "1234",
+          role: 0,
+          nickname: "난초",
+          phone: "01012341234",
+          birthday: "1111-11-11",
+          gender: 0,
+          height: 180,
+          region: "부산",
+          department: "컴공",
+          distance: 0,
+          smoking: 0,
+          drinking: 0,
+          introduction: "자기소개",
+          preference: "test",
+          nondepartment: "원하지 않는 단과대",
+          nonstudentid: "원하지 않는 학번",
+          nonage: "원하지 않는 나이"
+      };
+      
+      formData.append("request", JSON.stringify(values));
       
 
-      const checkWrongPW = (e)=>{
-        setPasswordCheck(e.target.value);
-        if(values.password !== passwordCheck){
-          setWrongPW(false)
-        }
-        else setWrongPW(true);
-      }
 
-      const handleChange = e => {
-        setValues({
-          ...values,
-          [e.target.name]: e.target.value,
+      axios
+        .post(`http://13.209.145.28:8080/api/v1/users`,formData , {
+          headers: {
+            'Content-Type': "multipart/form-data",
+          },
         })
-        console.log(values);
-      }
-    
-      const handleSubmit = e => {
-        e.preventDefault()
-        console.log('실행');
-        alert(JSON.stringify(values, null, 2))
-      }
-
-
-    const handleCancleButton = (event) => {
-        //취소 시 이전 페이지로
-        navigate(-1);
-    }
-    const handleSubmitButton = (event) => {
-        //가입완료, 정보들 서버로 전송하는 api 필요
-    }
-
-    return (
-        <>  
-            <Title title = "회원가입"/>
-            <Separator />
-
-            <AppContainer>
-                <InfoMessage>먼저 프로필 사진을 등록해주세요 </InfoMessage>
-                <LogoContainer>
-                    <ProfileImage></ProfileImage>
-                </LogoContainer>
-
-                <InfoMessage>아이디와 비밀번호를 입력해주세요</InfoMessage>
-                <LoginForm onSubmit={handleSubmit}>
-                    <Input type="text" name="email" value={values.email} onChange={handleChange} placeholder="이메일"/>
-                    <Input type="password" name="password" value={values.password} onChange={handleChange} placeholder="패스워드" />
-                    <Input type="password" value={values.passwordCheck} onBlur={checkWrongPW} placeholder="패스워드 확인" />
-                    {wrongPW?<p>비밀번호가 틀립니다.</p>: ''}
-                    <InfoMessage>기본 정보를 작성해주세요.</InfoMessage>
-                <InfoName>닉네임</InfoName>
-                <Input type="text" name="nickname" value={values.nickname} onChange={handleChange}  placeholder="닉네임을 입력해주세요."/>
-
-                <InfoName>연락처</InfoName>
-                    <Input type="tel" name="phone" value={values.phone} onChange={handleChange}  placeholder="***-****-****"/>
-
-                <InfoName>생년월일</InfoName>
-                    <Input type="date" name="birthday" value={values.birthday} onChange={handleChange} />
-
-                <InfoName>성별</InfoName>
-                <CircleCheckbox options={['여성', '남성']} name='gender' value={values.gender} onChange={handleChange}/>
-
-                <InfoName>키</InfoName>
-                    <Input type="number" name='height' value={values.height} onChange={handleChange}  placeholder="***cm"/>
-
-                <InfoName>지역</InfoName>
-                    <Input type="text" name='region' value={values.region} onChange={handleChange}  placeholder="대략적인 **구까지만 써주세요."/>
-
-                <InfoName>장거리 가능 유무</InfoName>
-                <CircleCheckbox options={['가능', '불가능']} name='distance' value={values.distance} onChange={handleChange}/>
-
-                <InfoName>흡연 여부</InfoName>
-                <CircleCheckbox options={['흡연', '비흡연']} name='smoking' value={values.smoking} onChange={handleChange}/>
-
-                <InfoName>음주 여부</InfoName>
-                <CircleCheckbox options={['음주', '비음주']} name='drinking' value={values.drinking} onChange={handleChange} />
-                    
-                <InfoName>내가 속한 단과대는?</InfoName>
-                    <Input type="text" name="department" value={values.department} onChange={handleChange}/>
-
-                <InfoName>자기소개를 입력해주세요.</InfoName>
-                    <Input type="text" name='introduction' value={values.introduction} onChange={handleChange}/>
-
-                <InfoName>나의 이상형에 대해서 알려주세요!</InfoName>
-                    <Input type="text" name='preference' value={values.preference} onChange={handleChange} placeholder='자세하게 적어주실 수록 좋아요!' />
-
-                <InfoName>매칭되고 싶지 않은 학과를 적어주세요! 없으면 없음이라고 적어주세요.</InfoName>
-                    <Input type="text" name='nondepartment' value={values.nondepartment} onChange={handleChange}/>
-
-                <InfoName>매칭되고 싶지 않은 학번를 적어주세요! (예, 20학번) 없으면 없음이라고 적어주세요.</InfoName>
-                    <Input type="text" name='nonstudentid' value={values.nonstudentid} onChange={handleChange} />
-
-                <InfoName>매칭되고 싶지 않은 나이를 적어주세요! (예, 2005년생보다 어린 나이) 없으면 없음이라고 적어주세요.</InfoName>
-                    <Input type="text" name='nonage' value={values.nonage} onChange={handleChange}/>
-
-                <InfoName>매칭되고 싶지 않은 지역 조건을 적어주세요! (예, 부산 외에 지역만 매칭 원함) 없으면 없음이라고 적어주세요.</InfoName>
-                    <Input type="text" name='nonregion' value={values.nonregion} onChange={handleChange}/>
-                <ButtonContainer>
-                    <CancelButton onClick={handleCancleButton}>취소</CancelButton>
-                    <SubmitButton   type="submit" onClick={handleSubmitButton}>이어서 가입하기</SubmitButton>
-                </ButtonContainer>
-                </LoginForm>
-            </AppContainer>
-            <FooterContainer>
-                <Footer/>
-            </FooterContainer>
-        </>
-    )
+        .then(function (response) {
+          // 성공적으로 응답 받았을 때의 처리
+          console.log("응답 데이터:", response.data);
+          navigate("/JudgePage");
+        })
+        .catch(function (error) {
+          // 오류 발생 시의 처리
+          console.error("오류 발생:", error);
+          console.log(request);
+          let values = formData.keys();
+          for (const pair of values) {
+              console.log(pair,'폼데이터'); 
 }
+          
+        });
+    }
+    
+  };
+
+  return (
+    <>
+      <Title title="회원가입" />
+      <Separator />
+
+      <AppContainer>
+        <InfoMessage>먼저 프로필 사진을 등록해주세요 </InfoMessage>
+        <LogoContainer>
+          <ProfileImage onFileChange={handleFileChange} selectedFile={selectedFile}/>
+        </LogoContainer>
+
+        <InfoMessage>아이디와 비밀번호를 입력해주세요</InfoMessage>
+        <LoginForm onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            placeholder="이메일"
+          />
+          <Input
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            placeholder="패스워드"
+            autoComplete="new-password"
+          />
+          <Input
+            type="password"
+            value={values.passwordCheck}
+            onBlur={checkWrongPW}
+            placeholder="패스워드 확인"
+            autoComplete="new-password"
+          />
+          {wrongPW ? <p>비밀번호가 틀립니다.</p> : ""}
+          <InfoMessage>기본 정보를 작성해주세요.</InfoMessage>
+          <InfoName>닉네임</InfoName>
+          <Input
+            type="text"
+            name="nickname"
+            value={values.nickname}
+            onChange={handleChange}
+            placeholder="닉네임을 입력해주세요."
+          />
+
+          <InfoName>연락처</InfoName>
+          <Input
+            type="tel"
+            name="phone"
+            value={values.phone}
+            onChange={handleChange}
+            placeholder="***-****-****"
+          />
+
+          <InfoName>생년월일</InfoName>
+          <Input
+            type="date"
+            name="birthday"
+            value={values.birthday}
+            onChange={handleChange}
+          />
+
+          <InfoName>성별</InfoName>
+          <CircleCheckbox
+            options={["여성", "남성"]}
+            name="gender"
+            value={values.gender}
+            onChange={handleChange}
+          />
+
+          <InfoName>키</InfoName>
+          <Input
+            type="number"
+            name="height"
+            value={values.height}
+            onChange={handleChange}
+            placeholder="***cm"
+          />
+
+          <InfoName>지역</InfoName>
+          <Input
+            type="text"
+            name="region"
+            value={values.region}
+            onChange={handleChange}
+            placeholder="대략적인 **구까지만 써주세요."
+          />
+
+          <InfoName>장거리 가능 유무</InfoName>
+          <CircleCheckbox
+            options={["가능", "불가능"]}
+            name="distance"
+            value={values.distance}
+            onChange={handleChange}
+          />
+
+          <InfoName>흡연 여부</InfoName>
+          <CircleCheckbox
+            options={["흡연", "비흡연"]}
+            name="smoking"
+            value={values.smoking}
+            onChange={handleChange}
+          />
+
+          <InfoName>음주 여부</InfoName>
+          <CircleCheckbox
+            options={["음주", "비음주"]}
+            name="drinking"
+            value={values.drinking}
+            onChange={handleChange}
+          />
+
+          <InfoName>내가 속한 단과대는?</InfoName>
+          <Input
+            type="text"
+            name="department"
+            value={values.department}
+            onChange={handleChange}
+          />
+
+          <InfoName>자기소개를 입력해주세요.</InfoName>
+          <LimitInputForm
+            name="introduction"
+            value={values.introduction}
+            onChange={handleChange}
+          />
+          <InfoName>나의 이상형에 대해서 알려주세요!</InfoName>
+          <LimitInputForm
+            name="preference"
+            value={values.preference}
+            onChange={handleChange}
+          />
+          <InfoName>
+            매칭되고 싶지 않은 학과를 적어주세요! 없으면 없음이라고 적어주세요.
+          </InfoName>
+          <Input
+            type="text"
+            name="nondepartment"
+            value={values.nondepartment}
+            onChange={handleChange}
+          />
+
+          <InfoName>
+            매칭되고 싶지 않은 학번를 적어주세요! (예, 20학번) 없으면 없음이라고
+            적어주세요.
+          </InfoName>
+          <Input
+            type="text"
+            name="nonstudentid"
+            value={values.nonstudentid}
+            onChange={handleChange}
+          />
+
+          <InfoName>
+            매칭되고 싶지 않은 나이를 적어주세요! (예, 2005년생보다 어린 나이)
+            없으면 없음이라고 적어주세요.
+          </InfoName>
+          <Input
+            type="text"
+            name="nonage"
+            value={values.nonage}
+            onChange={handleChange}
+          />
+
+          <InfoName>
+            매칭되고 싶지 않은 지역 조건을 적어주세요! (예, 부산 외에 지역만
+            매칭 원함) 없으면 없음이라고 적어주세요.
+          </InfoName>
+          <Input
+            type="text"
+            name="nonRegion"
+            value={values.nonRegion}
+            onChange={handleChange}
+          />
+          <ButtonContainer>
+            <CancelButton onClick={handleCancleButton}>취소</CancelButton>
+            <SubmitButton type="submit" onClick={handleSubmitButton}>
+              이어서 가입하기
+            </SubmitButton>
+          </ButtonContainer>
+        </LoginForm>
+      </AppContainer>
+      <FooterContainer>
+        <Footer />
+      </FooterContainer>
+    </>
+  );
+};
 
 export default Signup;
 
-
 // 전체를 담고 있는 컨테이너
 const AppContainer = styled.div`
-    justify-content: center;
-    width: 40%; /* 원하는 크기로 조정 (가로의 반 정도로 설정) */
-    margin: 0 auto; /* 수평 가운데 정렬을 위해 margin을 auto로 설정 */
+  justify-content: center;
+  width: 40%; /* 원하는 크기로 조정 (가로의 반 정도로 설정) */
+  margin: 0 auto; /* 수평 가운데 정렬을 위해 margin을 auto로 설정 */
 `;
 
 //ProfileBasic 로고 표시를 위한 스타일
@@ -160,21 +335,20 @@ const LogoContainer = styled.div`
   justify-content: center; /* 가로축에서 가운데 정렬 */
 `;
 
-
 //수평선 스타일
 const Separator = styled.div`
   height: 1px;
   background-color: Gray;
-  margin: 100px; 
+  margin: 100px;
 `;
 
 //어떤 정보를 입력해야하는지, 알려주는 텍스트
 const InfoMessage = styled.div`
-    text-align: center;
-    margin-top : 100px;
-    margin-bottom : 20px;
-    font-size: 25px; 
-    width: 100%; /* 텍스트가 max-width를 넘어가더라도 크기를 조절할 수 있도록 */
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 20px;
+  font-size: 25px;
+  width: 100%; /* 텍스트가 max-width를 넘어가더라도 크기를 조절할 수 있도록 */
 `;
 
 // 이메일과 패스워드를 입력할 칸들
@@ -192,11 +366,10 @@ const Input = styled.input`
 
 // 칸 마다 입력해야하는 정보 알려주는 작은 텍스트
 const InfoName = styled.div`
-    font-size: 15px;
+  font-size: 15px;
 `;
 
-
-//취소, 이어서 가입 버튼 
+//취소, 이어서 가입 버튼
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-between; /* 버튼을 좌우로 나눕니다. */
@@ -210,7 +383,7 @@ const CancelButton = styled.button`
 `;
 
 const SubmitButton = styled.button`
-  background-color: #23CAFF;
+  background-color: #23caff;
   color: white;
   border: none;
   padding: 10px;
@@ -219,7 +392,7 @@ const SubmitButton = styled.button`
 
   /* isFormValid가 언제 false로 설정할지 필요 */
   &:disabled {
-    background-color: #B9EEFF;
+    background-color: #b9eeff;
     cursor: not-allowed;
   }
 `;
