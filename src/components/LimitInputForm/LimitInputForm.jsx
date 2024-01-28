b961946b1f22f8eea4d23fab0a96995442706b66
dani001024 <aj9473@naver.com>
2024-01-28 23:18:57 +0900

    입력 사항 Singup 컴포넌트에서 사용 가능하도록 수정

--- src/components/LimitInputForm/LimitInputForm.jsx
+++ src/components/LimitInputForm/LimitInputForm.jsx
@@ -3,20 +3,19 @@ import styled from 'styled-components';
 
 const MAX_LENGTH = 110;
 
-const LimitInputForm = () => {
-  const [text, setText] = useState('');
+const LimitInputForm = ({name,value,onChange}) => {
 
-  const handleTextChange = (e) => {
-    const value = e.target.value;
-    if (value.length <= MAX_LENGTH) {
-      setText(value);
-    }
-  };
+  // const handleTextChange = (e) => {
+  //   const inputValue = e.target.value;
+  //   if (inputValue.length <= MAX_LENGTH) {
+  //     onChange(name,inputValue); // 상위 컴포넌트의 onChange 함수 호출
+  //   }
+  // };
 
   return (
     <Container>
-      <Textarea value={text} onChange={handleTextChange} placeholder="최대 110자까지 입력할 수 있습니다." />
-      <Counter>{`${text.length} / ${MAX_LENGTH}`}</Counter>
+      <Textarea value={value} onChange={(e) => onChange({ target: { name, value: e.target.value } })} placeholder="최대 110자까지 입력할 수 있습니다." />
+      <Counter>{`${value.length} / ${MAX_LENGTH}`}</Counter>
     </Container>
   );
 };
