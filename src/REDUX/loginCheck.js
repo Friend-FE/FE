08fbc869cecef4347743f49230a8f503fa485e9c
dani001024 <aj9473@naver.com>
2024-02-08 21:52:58 +0900

    로그인 여부 리덕스에 저장

--- src/REDUX/loginCheck.js
+++ src/REDUX/loginCheck.js
@@ -2,10 +2,18 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const loginSlice = createSlice({
     name: "login",
-    initialState: ,
+    initialState: {
+        isLoggedIn: false 
+    },
     reducers: {
+        login: state => {
+            state.isLoggedIn = true; // 로그인 액션
+        },
+        logout: state => {
+            state.isLoggedIn = false;
+        } // 로그아웃 액션
     },
 });
 
-export const {} = loginSlice.actions;
+export const {login, logout} = loginSlice.actions;
 export default loginSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './REDUX/store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
 
